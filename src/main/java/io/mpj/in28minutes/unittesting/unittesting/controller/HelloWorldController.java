4b1ceec6614b146d5301ca8c5eb8946ddb447848
Abdurrachman Mappuji <11813607+empeje@users.noreply.github.com>
2022-12-31 20:46:47 +0100

    Add test for hello world controller

--- src/main/java/io/mpj/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
+++ src/main/java/io/mpj/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package io.mpj.in28minutes.unittesting.unittesting;
+package io.mpj.in28minutes.unittesting.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/io/mpj/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -0,0 +1,34 @@
+package io.mpj.in28minutes.unittesting.unittesting.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorld_basic() throws Exception {
+        // call GET "/hello-world" application/json
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);;
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        // verify "Hello World"
+        assertEquals("Hello World!", result.getResponse().getContentAsString());
+    }
+
+}
