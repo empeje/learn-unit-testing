2c0e201d95e563b6db7ca809225f641c28bc6bed
Abdurrachman Mappuji <11813607+empeje@users.noreply.github.com>
2022-12-31 20:51:42 +0100

    Use andExpect

--- src/test/java/io/mpj/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
+++ src/test/java/io/mpj/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloWorldController.class)
@@ -25,10 +27,13 @@ public class HelloWorldControllerTest {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/hello-world")
                 .accept(MediaType.APPLICATION_JSON);;
-        MvcResult result = mockMvc.perform(request).andReturn();
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"))
+                .andReturn();
 
         // verify "Hello World"
-        assertEquals("Hello World!", result.getResponse().getContentAsString());
+        // assertEquals("Hello World!", result.getResponse().getContentAsString());
     }
 
 }
