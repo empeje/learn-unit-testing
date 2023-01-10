6677a463aff2f0ef812f2dc88f39ded3653fc244
Abdurrachman Mappuji <11813607+empeje@users.noreply.github.com>
2023-01-10 20:20:15 +0100

    Creating different test configuration

--- src/test/java/io/mpj/in28minutes/unittesting/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/io/mpj/in28minutes/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -2,8 +2,10 @@ package io.mpj.in28minutes.unittesting.unittesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:test-configuration.properties"})
 class UnitTestingApplicationTests {
 
 	@Test
